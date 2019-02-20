b48b8054b588e299adc9493a0eee9b67ff984332
quangbruder <quangbruder@gmail.com>
2019-02-20 03:47:51 +0100

    Initial Commit

---
+++ Nhap tich Duc Einbuegerungstest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nhap tich Duc Einbuegerungstest
+//
+//  Created by Nguyen Quang on 20.02.19.
+//  Copyright © 2019 Nguyen Quang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
